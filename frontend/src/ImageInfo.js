01c48a51f519df87b8c30b04d5e3cf6612301a61
sryung <sryung1225@gmail.com>
2023-07-03 19:03:07 +0900

    [1-4] 모달 제어 - 고양이 상세 정보(성격, 태생정보) 렌더링 : fetchCatDetail - showDetail - 다크모드에 맞게 css 수정 - 모달 닫기 : 닫기 버튼 클릭 / esc 키다운 / 모달 외 영역 클릭

--- frontend/src/ImageInfo.js
+++ frontend/src/ImageInfo.js
@@ -18,9 +18,27 @@ class ImageInfo {
     this.render();
   }
 
+  showDetail(data) {
+    // 상세 정보 요청
+    api.fetchCatDetail(data.cat.id).then(({ data }) => {
+      // 정보 업데이트
+      this.setState({
+        visible: true,
+        cat: data,
+      });
+    });
+  }
+
+  closeImageInfo() {
+    this.setState({
+      visible: false,
+      cat: undefined,
+    });
+  }
+
   render() {
     if (this.data.visible) {
-      const { name, url, temperament, origin } = this.data.image;
+      const { name, url, temperament, origin } = this.data.cat;
 
       this.$imageInfo.innerHTML = `
         <div class="content-wrapper">
@@ -28,13 +46,26 @@ class ImageInfo {
             <span>${name}</span>
             <div class="close">x</div>
           </div>
-          <img src="${url}" alt="${name}"/>        
+          <img src="${url}" alt="${name}"/>
           <div class="description">
             <div>성격: ${temperament}</div>
             <div>태생: ${origin}</div>
           </div>
         </div>`;
       this.$imageInfo.style.display = "block";
+      document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+          this.closeImageInfo();
+        }
+      });
+      this.$imageInfo.addEventListener("click", (e) => {
+        if (
+          e.target.className === "ImageInfo" ||
+          e.$target.className === "close"
+        ) {
+          this.closeImageInfo();
+        }
+      });
     } else {
       this.$imageInfo.style.display = "none";
     }
